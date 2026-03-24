d0a733d5e8497b02bba6f2b2702c1964a675efe2
berezney <berezney@brandeis.edu>
2026-03-24 14:42:35 -0400

    Align spatial split notebook with CV config

--- config/cv_params.py
+++ config/cv_params.py
@@ -1,6 +1,14 @@
-# parameters for the nested cv 
+#parameters for initial spatial (outer) cv fold
+N_SPATIAL_CLUSTERS = 200 # number of k-means clusters that we use to spatially divide Canada
+# 200 nicely separates Toronto and Missasauga without overly subdividing
+
+NUM_TEST_SPLITS = 5  # KFOLD SPLITTING NEEDS AN INTEGER: 1/N gives the fractional size of the test split
+NUM_CV_SPLITS = 5  # Number of folds for kfold splitting
 
 OUTER_FOLD_COLUMN = "cv_fold"
+
+# parameters for the nested cv 
+
 INNER_CV_GROUP_COLUMN = "spatial_cluster"
 INNER_CV_STRATIFY_COLUMN = "provinceterritory"
 TEST_FLAG_COLUMN = "is_test"
